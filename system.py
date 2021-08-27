d6fad5c51d661dee85c0e5d67b299b41be5a44da
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-08-27 01:10:52 -0300

    def hit_or_stand

--- system.py
+++ system.py
@@ -19,3 +19,22 @@ def hit(deck, hand):
     single_card = deck.deal()
     hand.add_card(single_card)
     hand.adjust_for_ace()
+
+
+def hit_or_stand(deck, hand):
+    global playing # Controla um while Loop futuro
+
+    while True:
+        x = input('Comprar ou Parar? ').strip().lower()
+
+        if x[0] == 'c':
+            hit(deck, hand)
+
+        elif x[0] == 'p':
+            print('Jogador parou, turno do Dealer')
+            playing == False
+
+        else:
+            print('Desculpe, eu não entedi, por favor informe se deseja continuar ou parar.')
+            continue
+        break
