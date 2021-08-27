82d6873cb39a99b591104ebff26ec53dd2cea3ba
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-08-27 00:56:55 -0300

    Added Class Hand

--- stuff/__init__.py
+++ stuff/__init__.py
@@ -34,5 +34,31 @@ class Deck:
         random.shuffle(self.deck)
 
     def deal(self):
-        single_card = self.deck.pop()
+        single_card = self.deck.pop() # Pop last card from deck
         return single_card
+
+
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0  # start with zero value
+        self.aces = 0  # add an attribute to keep track of aces
+
+    def add_card(self, card):
+        # card passed in
+        # from Deck.deal() --: single Card()
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        # Track ACEs
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_for_ace(self):
+
+        # IF Total value > 21 and I still have an ACE
+        # THAN CHANGE my ACE to be a 1 instead of an 11
+        while self.value > 21 and self.aces > 0:
+            self.value -= 10
+            self.aces -= 1
+
